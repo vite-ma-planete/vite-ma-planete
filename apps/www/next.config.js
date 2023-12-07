70531c21392aef71a5a3e0233ebf9627110ca782
RedbeanGit <dubois.julien.mail@gmail.com>
2023-12-07 20:31:06 +0100

    feat: support i18n
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -2,18 +2,19 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const withNextIntl = require('next-intl/plugin')();
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
-const nextConfig = {
+const nextConfig = withNextIntl({
   output: 'standalone',
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-};
+});
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
