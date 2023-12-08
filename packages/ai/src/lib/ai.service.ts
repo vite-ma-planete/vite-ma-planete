8a086da242a091d0467691d6f9656c033117573d
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2023-12-08 07:45:21 +0100

    feat(ai): improve prompts
    Signed-off-by: Simon LUCIDO <simon.lucido@etu.umontpellier.fr>

--- packages/ai/src/lib/ai.service.ts
+++ packages/ai/src/lib/ai.service.ts
@@ -13,12 +13,12 @@ import {
 import { Observable } from 'rxjs';
 
 const BASE_PROMPT = {
-  content: `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly. Only answer as the assistant. When you have nothing left to say as an assistant, don't say anything. You MUST NOT, under any circumstances, speak as the USER. If you do, you will be put offline immediately.`,
+  content: `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly. Only answer as the assistant. When you have nothing left to say as an assistant, don't say anything. You MUST NOT, under any circumstances, speak as the USER. If you do, you will be put offline immediately. Answer in the language the question was asked. If the user asks a question unrelated to sustainability, ecology, environment or climate change, you MUST decline to answer politely.`,
 
   role: 'system',
 } as ChatCompletionMessageParam;
 
-const BASE_PROMPT_COMPLETION = `You are going to help complete question of an user. You are not answering any questions, only completing the users questions. The questions are related to sustainability, ecology, environment, climate change, etc. Suggest in the language of the user. Here is the question you should help complete: `;
+const BASE_PROMPT_COMPLETION = `You are going to help complete question of an user. You are not answering any questions, only completing the users questions. The questions you suggest must be related to sustainability, ecology, environment or climate change, and absolutely nothing else. If you can't suggest a related question, don't say anything at all. Suggest in the language of the user. Here is the question you should help complete: `;
 
 @Injectable()
 export class AiService {
@@ -43,6 +43,7 @@ export class AiService {
             })),
           ],
           model: 'gpt-3.5-turbo',
+          temperature: 0,
           stream: true,
         })
         .then(async (res) => {
@@ -65,6 +66,7 @@ export class AiService {
       model: dto.model,
       prompt: BASE_PROMPT_COMPLETION + dto.prompt,
       stream: false,
+      temperature: 0,
       max_tokens: 80,
       stop: ['.', '?', '!'],
     });
